717fbcbb0d0312a2ee40698606bab900971cb8bc
Adam <Adam.Murray@blackbay.com>
2016-12-19 16:51:54 +1300

    Changes: Change the inteval option to include the seconds in the varible name for clarity.
    Change the request-promise call to use the options not just the url.
    allowing for more control, and the ability to specify headers

--- README.md
+++ README.md
@@ -9,13 +9,19 @@ All you need is a `.json` file with the following configurations set up.
 This is loaded from the `hubotrssconfig.json` file in the top level of the hubot
 install, or you can specify in an environment variable `HUBOT_RSS_CONFIG_FILE`.
 
+
 ```json
 {
   "feeds": [{
     "name": "Name of the RSS feed goes here",
-    "url": "URL to the RSS feed",
+    "request": {
+      "uri": "URI to the RSS feed",
+      "headers": {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"
+      }
+    },
     "room": "room to message out to when an update is found",
-    "pingInterval": "100", // How many seconds to wait before polling for update
+    "pingIntervalSeconds": "100", 
     "alertPrefix": "A prefix to the output message goes here.",
     "alertSuffix": "a suffix to an output message goes here."
   }]
--- src/rss-feed-poller.js
+++ src/rss-feed-poller.js
@@ -2,46 +2,46 @@ import NodePie from 'nodepie';
 import request from 'request-promise';
 
 export default function getFeed(options) {
-  let lastTime = 0;
-  async function checkFeed() {
-    const time = new Date().getTime();
-    options.robot.logger.debug(`Checking ${options.name || 'unnamed feed'} at ${time}`);
+    let lastTime = 0;
+    async function checkFeed() {
+        const time = new Date().getTime();
+        options.robot.logger.debug(`Checking ${options.name || 'unnamed feed'} at ${time}`);
 
-    const requestResult = await request(options.url);
+        const requestResult = await request(options.request);
 
-    const feedResult = new NodePie(requestResult);
-    try {
-      feedResult.init();
-    } catch (err) {
-      options.robot.logger.debug(`${err.message}`);
+        const feedResult = new NodePie(requestResult);
+        try {
+            feedResult.init();
+        } catch (err) {
+            options.robot.logger.debug(`${err.message}`);
+        }
+
+        const latestItem = feedResult.getItem(0);
+        if (latestItem) {
+            const itemPostedTime = latestItem.getUpdateDate();
+            options.robot.logger.debug(`${itemPostedTime}`);
+            if (itemPostedTime >= lastTime) {
+                options.robot.logger.debug(`Found update for: ${latestItem.getTitle()}`);
+                options.robot.messageRoom(
+                    options.room,
+                    `${options.alertPrefix || ''}${latestItem.getTitle()} - ${latestItem.getPermalink()}` +
+                    `${options.alertSuffix || ''}`
+                );
+            }
+        }
+
+        lastTime = time;
     }
 
-    const latestItem = feedResult.getItem(0);
-    if (latestItem) {
-      const itemPostedTime = latestItem.getUpdateDate();
-      options.robot.logger.debug(`${itemPostedTime}`);
-      if (itemPostedTime >= lastTime) {
-        options.robot.logger.debug(`Found update for: ${latestItem.getTitle()}`);
-        options.robot.messageRoom(
-          options.room,
-          `${options.alertPrefix || ''}${latestItem.getTitle()} - ${latestItem.getPermalink()}` +
-            `${options.alertSuffix || ''}`
-        );
-      }
+    function startFeed() {
+        options.robot.logger.info(`Starting feed poller for ${options.name}.`);
+        checkFeed();
+
+        setInterval(checkFeed, options.pingIntervalSeconds * 1000);
     }
 
-    lastTime = time;
-  }
-
-  function startFeed() {
-    options.robot.logger.info(`Starting feed poller for ${options.name}.`);
-    checkFeed();
-
-    setInterval(checkFeed, options.pingInterval * 1000);
-  }
-
-  return {
-    checkFeed,
-    startFeed,
-  };
+    return {
+        checkFeed,
+        startFeed,
+    };
 }
